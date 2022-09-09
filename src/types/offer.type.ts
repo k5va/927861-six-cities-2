816f3b4c5a76bdd5d6ee56b580d0411b8e712b47
k5va <k5va@yandex.ru>
2022-09-09 20:46:28 +0300

    Added types for offer and user

---
+++ src/types/offer.type.ts
@@ -0,0 +1,24 @@
+import { OfferType } from './offer-type.enum.js';
+import { User } from './user.type.js';
+
+export type Offer = {
+  title: string;
+  description: string;
+	publishDate: Date;
+	city: string;
+	previewImage: string;
+  images: string[];
+  isFavorite: boolean;
+  isPremium: boolean;
+  rating: number;
+  type: OfferType;
+  bedrooms: number;
+  maxAdults: number;
+  price: number;
+  goods: string[];
+  host: User;
+  longitude: number;
+  latitude: number;
+};
+
+export default Offer;
