f5031bbd5702ba920c92d8d3b004788d96323a10
k5va <k5va@yandex.ru>
2022-09-18 18:03:07 +0300

    Added LoggerInteface

---
+++ src/common/logger/logger.interface.ts
@@ -0,0 +1,6 @@
+export interface LoggerInterface {
+  log(msg: string, ...args: unknown[]): void;
+  warn(msg: string, ...args: unknown[]): void;
+  error(msg: string, ...args: unknown[]): void;
+  debug(msg: string, ...args: unknown[]): void;
+}
