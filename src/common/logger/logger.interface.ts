2674f50ab3a9e1285b27d6a46344505084b35bdc
k5va <k5va@yandex.ru>
2022-09-18 18:11:46 +0300

    Added LoggerService

--- src/common/logger/logger.interface.ts
+++ src/common/logger/logger.interface.ts
@@ -1,5 +1,5 @@
 export interface LoggerInterface {
-  log(msg: string, ...args: unknown[]): void;
+  info(msg: string, ...args: unknown[]): void;
   warn(msg: string, ...args: unknown[]): void;
   error(msg: string, ...args: unknown[]): void;
   debug(msg: string, ...args: unknown[]): void;
