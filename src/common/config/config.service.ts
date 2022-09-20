20747f63289ffa6a520aca460cb35bf268e294a2
k5va <k5va@yandex.ru>
2022-09-20 12:59:24 +0300

    Installed dotenv

--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -1,8 +1,24 @@
 import { ConfigInterface } from './config.interface';
+import { config, DotenvParseOutput } from 'dotenv';
+import { LoggerInterface } from '../logger/logger.interface';
+import { inject, injectable } from 'inversify';
+import { Component } from '../../types/index.js';
 
+@injectable()
 export default class ConfigService implements ConfigInterface {
+  private config: DotenvParseOutput;
 
-  private config: NodeJS.ProcessEnv = process.env;
+  constructor(@inject(Component.LoggerInterface) private logger: LoggerInterface) {
+
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
+    }
+
+    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    this.logger.info('.env file found and successfully parsed!');
+  }
 
   get(key: string): string | undefined {
     return this.config[key];
