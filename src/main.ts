c09e1541def24953b9973b1c73408e0c8a945894
k5va <k5va@yandex.ru>
2022-09-20 13:59:09 +0300

    Moved IOC container initialization to separate module

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,6 @@
-import 'reflect-metadata';
-import { Container } from 'inversify';
 import Application from './app/application.js';
-import LoggerService from './common/logger/logger-service.js';
-import { LoggerInterface } from './common/logger/logger.interface.js';
+import { appContainer } from './common/container/container.js';
 import { Component } from './types/index.js';
-import { ConfigInterface } from './common/config/config.interface.js';
-import ConfigService from './common/config/config.service.js';
-
-const appContainer = new Container();
-appContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
-appContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
-appContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
 
 const app = appContainer.get<Application>(Component.Application);
 app.init();
