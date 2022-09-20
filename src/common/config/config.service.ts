923f138a2cd407923e52eb7358b10ab9b35800b1
k5va <k5va@yandex.ru>
2022-09-20 13:42:59 +0300

    Added convict Env validation

--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -1,12 +1,13 @@
 import { ConfigInterface } from './config.interface';
-import { config, DotenvParseOutput } from 'dotenv';
+import { config } from 'dotenv';
 import { LoggerInterface } from '../logger/logger.interface';
 import { inject, injectable } from 'inversify';
 import { Component } from '../../types/index.js';
+import {configSchema, ConfigSchema} from './config.schema.js';
 
 @injectable()
 export default class ConfigService implements ConfigInterface {
-  private config: DotenvParseOutput;
+  private config: ConfigSchema;
 
   constructor(@inject(Component.LoggerInterface) private logger: LoggerInterface) {
 
@@ -16,11 +17,14 @@ export default class ConfigService implements ConfigInterface {
       throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
     }
 
-    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    configSchema.load({}); // read values from env
+    configSchema.validate({allowed: 'strict', output: this.logger.info}); // validate loaded values
+    this.config = configSchema.getProperties(); // create corresponding object
+
     this.logger.info('.env file found and successfully parsed!');
   }
 
-  get(key: string): string | undefined {
+  public get<T extends keyof ConfigSchema>(key: T): ConfigSchema[T] {
     return this.config[key];
   }
 }
