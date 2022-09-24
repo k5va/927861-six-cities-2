079d439ad6e135049233e4fdd1713e3645899488
k5va <k5va@yandex.ru>
2022-09-24 23:50:59 +0300

    Added DB params to ConfigSchema

--- src/common/config/config.schema.ts
+++ src/common/config/config.schema.ts
@@ -5,8 +5,12 @@ convict.addFormats(validator);
 
 export type ConfigSchema = {
   PORT: number;
-  DB_HOST: string;
   SALT: string;
+  DB_HOST: string;
+  DB_USER: string;
+  DB_PASSWORD: string;
+  DB_PORT: number;
+  DB_NAME: string;
 }
 
 export const configSchema = convict<ConfigSchema>({
@@ -27,5 +31,29 @@ export const configSchema = convict<ConfigSchema>({
     format: 'ipaddress',
     env: 'DB_HOST',
     default: '127.0.0.1'
+  },
+  DB_USER: {
+    doc: 'Database user',
+    format: String,
+    env: 'DB_USER',
+    default: null
+  },
+  DB_PASSWORD: {
+    doc: 'Database password',
+    format: String,
+    env: 'DB_PASSWORD',
+    default: null
+  },
+  DB_PORT: {
+    doc: 'Database port',
+    format: 'port',
+    env: 'DB_PORT',
+    default: 27017
+  },
+  DB_NAME: {
+    doc: 'Database name',
+    format: String,
+    env: 'DB_NAME',
+    default: 'six-cities'
   },
 });
