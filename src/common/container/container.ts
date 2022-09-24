18a7fd79505c9d427388ca581663970dbcd69b5a
k5va <k5va@yandex.ru>
2022-09-25 00:19:35 +0300

    Added DataBaseService to IOC container

--- src/common/container/container.ts
+++ src/common/container/container.ts
@@ -6,11 +6,14 @@ import { ConfigInterface } from '../config/config.interface.js';
 import ConfigService from '../config/config.service.js';
 import LoggerService from '../logger/logger-service.js';
 import { LoggerInterface } from '../logger/logger.interface.js';
+import { DatabaseInterface } from '../database-client/database.interface.js';
+import DatabaseService from '../database-client/database.service.js';
 
 const appContainer = new Container();
 
 appContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
 appContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 appContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
+appContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
 
 export { appContainer };
--- src/types/component.ts
+++ src/types/component.ts
@@ -2,6 +2,7 @@ const Component = {
   Application: Symbol.for('Application'),
   LoggerInterface: Symbol.for('LoggerInterface'),
   ConfigInterface: Symbol.for('ConfigInterface'),
+  DatabaseInterface: Symbol.for('DatabaseInterface'),
 } as const;
 
 export default Component;
