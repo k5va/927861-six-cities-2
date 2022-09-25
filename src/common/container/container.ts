38b98064d0edc69a3da142ffa00abda25baaea1f
k5va <k5va@yandex.ru>
2022-09-25 10:19:42 +0300

    Added user service

--- src/common/container/container.ts
+++ src/common/container/container.ts
@@ -8,6 +8,10 @@ import LoggerService from '../logger/logger-service.js';
 import { LoggerInterface } from '../logger/logger.interface.js';
 import { DatabaseInterface } from '../database-client/database.interface.js';
 import DatabaseService from '../database-client/database.service.js';
+import { UserServiceInterface } from '../../modules/user/user-service.interface.js';
+import UserService from '../../modules/user/user.service.js';
+import { types } from '@typegoose/typegoose';
+import { UserEntity, UserModel } from '../../modules/user/user.entity.js';
 
 const appContainer = new Container();
 
@@ -15,5 +19,7 @@ appContainer.bind<Application>(Component.Application).to(Application).inSingleto
 appContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 appContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
 appContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
+appContainer.bind<UserServiceInterface>(Component.DatabaseInterface).to(UserService).inSingletonScope();
+appContainer.bind<types.ModelType<UserEntity>>(Component.UserModel).toConstantValue(UserModel);
 
 export { appContainer };
--- src/types/component.ts
+++ src/types/component.ts
@@ -3,6 +3,8 @@ const Component = {
   LoggerInterface: Symbol.for('LoggerInterface'),
   ConfigInterface: Symbol.for('ConfigInterface'),
   DatabaseInterface: Symbol.for('DatabaseInterface'),
+  UserServiceInterface: Symbol.for('UserServiceInterface'),
+  UserModel: Symbol.for('UserModel'),
 } as const;
 
 export default Component;
