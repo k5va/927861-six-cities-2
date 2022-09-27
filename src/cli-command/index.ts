371abf62e792ccc2e2d16cfabce7b8fd8326a16e
k5va <k5va@yandex.ru>
2022-09-27 12:10:15 +0300

    Fixed imports in cli.ts

--- src/cli-command/index.ts
+++ src/cli-command/index.ts
@@ -2,4 +2,5 @@ export { default as CliCommandInterface } from './cli-command.interface.js';
 export { default as HelpCommand } from './help-command/help-command.js';
 export { default as VersionCommand } from './version-command/version-command.js';
 export { default as ImportCommand } from './import-command/import-command.js';
+export { default as GenerateCommand } from './generate-command/generate-command.js';
 export { default as CommandNames } from './command-names.enum.js';
