bf393b635523612b9553291a3efabc885e996384
k5va <k5va@yandex.ru>
2022-10-10 13:31:18 +0500

    Added controller interface and abstract controller

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,9 @@
-export { default as Offer} from './offer.type.js';
-export { default as OfferType} from './offer-type.enum.js';
-export { default as User} from './user.type.js';
-export { default as MockData} from './mock-data.type.js';
-export { default as Component} from './component.js';
-export { default as Good} from './good.type.js';
-export { default as City} from './city.type.js';
+export { default as Offer } from './offer.type.js';
+export { default as OfferType } from './offer-type.enum.js';
+export { default as User } from './user.type.js';
+export { default as MockData } from './mock-data.type.js';
+export { default as Component } from './component.js';
+export { default as Good } from './good.type.js';
+export { default as City } from './city.type.js';
+export { default as HttpMethod } from './http-method.enum.js';
+export { default as RouteInterface } from './route.interface.js';
