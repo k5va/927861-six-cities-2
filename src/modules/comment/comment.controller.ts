55f6590c9a5b7b3e7b30aba378fd4c8ff53bedb7
k5va <k5va@yandex.ru>
2022-10-11 12:35:44 +0500

    Update create comment rest api spec

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -18,19 +18,19 @@ export default class CommentController extends Controller {
 
     this.logger.info('Registering routes for CommentController…');
     this.addRoute({ path: '/:offerId', method: HttpMethod.Get, handler: this.index });
-    this.addRoute({ path: '/:offerId', method: HttpMethod.Post, handler: this.create });
+    this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
   }
 
   public async create(
-    {body, params}: Request<Record<string, string>, Record<string, unknown>, CreateCommentDto>,
+    {body}: Request<Record<string, unknown>, Record<string, unknown>, CreateCommentDto>,
     res: Response,
   ): Promise<void> {
 
-    const comment = await this.commentService.create(body); // TODO: Need params???
+    const comment = await this.commentService.create(body);
     if (!comment) {
       throw new HttpError(
         StatusCodes.NOT_FOUND,
-        `Offer with id «${params.offerId}» doesn't exist.`,
+        `Offer with id «${body.offerId}» doesn't exist.`,
         'CommentController'
       );
     }
