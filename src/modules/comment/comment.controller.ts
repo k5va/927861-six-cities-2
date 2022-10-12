75363ad37f584201000c5a5e917d3ab6fd829647
k5va <k5va@yandex.ru>
2022-10-12 18:24:26 +0500

    Added types for comment controller

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,3 +1,4 @@
+import * as core from 'express-serve-static-core';
 import { Controller, LoggerInterface, HttpError } from '../../common/index.js';
 import { inject, injectable } from 'inversify';
 import { Component, HttpMethod } from '../../types/index.js';
@@ -8,6 +9,7 @@ import { CommentServiceInterface } from './comment-service.interface.js';
 import { StatusCodes } from 'http-status-codes';
 import { fillDTO } from '../../utils/index.js';
 import { OfferServiceInterface } from '../offer/offer-service.interface.js';
+import { IndexParams } from './comment.types.js';
 
 @injectable()
 export default class CommentController extends Controller {
@@ -41,8 +43,7 @@ export default class CommentController extends Controller {
   }
 
   public async index(
-    {params}: Request<Record<string, string>, Record<string, unknown>,
-    Record<string, unknown>>,
+    {params}: Request<core.ParamsDictionary | IndexParams>,
     res: Response,
   ): Promise<void> {
 
