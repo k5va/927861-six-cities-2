4899ac43154587b160495fb9473111fb1be44142
k5va <k5va@yandex.ru>
2022-10-12 18:52:25 +0500

    Added types for offer controller

--- src/modules/offer/offer.controller.ts
+++ src/modules/offer/offer.controller.ts
@@ -1,3 +1,4 @@
+import * as core from 'express-serve-static-core';
 import { Controller, LoggerInterface, HttpError } from '../../common/index.js';
 import { inject, injectable } from 'inversify';
 import { Component, HttpMethod } from '../../types/index.js';
@@ -10,6 +11,7 @@ import OfferResponse from './response/offer.response.js';
 import UpdateOfferDto from './dto/update-offer.dto.js';
 import OfferShortResponse from './response/offer-short.response.js';
 import { OfferEntity } from './offer.entity.js';
+import { DeleteParams, IndexQuery, ShowParams, UpdateParams } from './offer.types.js';
 
 @injectable()
 export default class OfferController extends Controller {
@@ -22,7 +24,7 @@ export default class OfferController extends Controller {
     this.logger.info('Registering routes for OfferController…');
     this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
     this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.index });
-    this.addRoute({ path: '/:offerId', method: HttpMethod.Get, handler: this.getDetails });
+    this.addRoute({ path: '/:offerId', method: HttpMethod.Get, handler: this.show });
     this.addRoute({ path: '/:offerId', method: HttpMethod.Put, handler: this.update });
     this.addRoute({ path: '/:offerId', method: HttpMethod.Delete, handler: this.delete });
   }
@@ -37,7 +39,7 @@ export default class OfferController extends Controller {
   }
 
   public async update(
-    {body, params}: Request<Record<string, string>, Record<string, unknown>, UpdateOfferDto>,
+    {body, params}: Request<core.ParamsDictionary | UpdateParams, Record<string, unknown>, UpdateOfferDto>,
     res: Response,
   ): Promise<void> {
 
@@ -47,7 +49,10 @@ export default class OfferController extends Controller {
     this.ok(res, fillDTO(OfferResponse, offer));
   }
 
-  public async delete({params}: Request, res: Response): Promise<void> {
+  public async delete(
+    {params}: Request<core.ParamsDictionary | DeleteParams>,
+    res: Response
+  ): Promise<void> {
 
     const offer = await this.offerService.delete(params.offerId);
     this.checkOfferPresence(params.offerId, offer);
@@ -56,18 +61,20 @@ export default class OfferController extends Controller {
   }
 
   public async index(
-    {query}: Request<Record<string, unknown>, Record<string, unknown>,
-    Record<string, unknown>>,
+    {query}: Request<unknown, unknown, unknown, IndexQuery>,
     res: Response,
   ): Promise<void> {
 
-    const count = query.count ? Number(query.count) : undefined;
+    const count = query.count ? query.count : undefined;
     this.logger.info(`Getting offers max count ${count}`);
     const offers = await this.offerService.find(count);
     this.ok(res, fillDTO(OfferShortResponse, offers));
   }
 
-  public async getDetails({params}: Request, res: Response): Promise<void> {
+  public async show(
+    {params}: Request<core.ParamsDictionary | ShowParams>,
+    res: Response
+  ): Promise<void> {
     this.logger.info(`Getting details for offer ${params.offerId}`);
 
     const offer = await this.offerService.findById(params.offerId);
