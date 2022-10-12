c5b50db52c47ab1765b037cf0e9ef613fd3ecd04
k5va <k5va@yandex.ru>
2022-10-12 21:57:06 +0500

    Added validation to CreateCommentDto

--- src/modules/comment/dto/create-comment.dto.ts
+++ src/modules/comment/dto/create-comment.dto.ts
@@ -1,7 +1,23 @@
+import { IsDateString, IsMongoId, Max, MaxLength,
+  Min, MinLength, IsNumber } from 'class-validator';
+import { CommentLength, Rating } from '../../../const/index.js';
+
 export default class CreateCommentDto {
+  @MinLength(CommentLength.Min, {message: `Minimum text length must be ${CommentLength.Min}`})
+  @MaxLength(CommentLength.Max, {message: `Maximum text length must be ${CommentLength.Max}`})
   public text!: string;
+
+  @IsDateString({}, {message: 'publishDate must be valid ISO date'})
   public publishDate!: Date;
+
+  @IsNumber({}, {message: 'rating must be a number'})
+  @Min(Rating.Min, {message: `Minimum maxAdults is ${Rating.Min}`})
+  @Max(Rating.Max, {message: `Maximum maxAdults is ${Rating.Max}`})
   public rating!: number;
+
+  @IsMongoId({message: 'userId must be a valid id'})
   public userId!: string;
+
+  @IsMongoId({message: 'offerId must be a valid id'})
   public offerId!: string;
 }
