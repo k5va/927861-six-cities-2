2487d7bf2862f6fc8c549e88607323b5a74d5e93
k5va <k5va@yandex.ru>
2022-10-16 14:02:32 +0500

    Updated user controller login

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -7,9 +7,11 @@ import { Request, Response } from 'express';
 import CreateUserDto from './dto/create-user.dto.js';
 import LoginUserDto from './dto/login-user.dto.js';
 import UserResponse from './response/user.response.js';
+import LoggedInUserResponse from './response/logged-in-user.response.js';
 import { UserServiceInterface } from './user-service.interface.js';
 import { StatusCodes } from 'http-status-codes';
-import { fillDTO } from '../../utils/index.js';
+import { createJWT, fillDTO } from '../../utils/index.js';
+import { JWT_ALGORITM } from './user.const.js';
 
 @injectable()
 export default class UserController extends Controller {
@@ -61,23 +63,26 @@ export default class UserController extends Controller {
   }
 
   public async login(
-    {body}: Request<Record<string, unknown>, Record<string, unknown>, LoginUserDto>
+    {body}: Request<Record<string, unknown>, Record<string, unknown>, LoginUserDto>,
+    res: Response,
   ): Promise<void> {
-    const existingUser = await this.userService.findByEmail(body.email);
+    const existingUser = await this.userService.verifyUser(body, this.configService.get('SALT'));
 
     if (!existingUser) {
       throw new HttpError(
         StatusCodes.UNAUTHORIZED,
-        `User with email ${body.email} not found.`,
+        `User with email ${body.email} unauthorized.`,
         'UserController',
       );
     }
 
-    throw new HttpError(
-      StatusCodes.NOT_IMPLEMENTED,
-      'Not implemented',
-      'UserController',
+    const token = await createJWT(
+      JWT_ALGORITM,
+      this.configService.get('JWT_SECRET'),
+      {email: existingUser.email, id: existingUser.id}
     );
+
+    this.ok(res, fillDTO(LoggedInUserResponse, {...existingUser, token}));
   }
 
   public async logout(): Promise<void> {
