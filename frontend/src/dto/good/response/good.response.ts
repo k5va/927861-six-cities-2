4eccc87f20c3ddb27e1898f2e38da75245ab2e55
k5va <k5va@yandex.ru>
2022-10-22 14:10:40 +0500

    Add Offer adapter

--- frontend/src/dto/good/response/good.response.ts
+++ frontend/src/dto/good/response/good.response.ts
@@ -1,9 +1,4 @@
-import { Expose } from 'class-transformer';
-
 export default class GoodResponse {
-  @Expose()
   public id!: string;
-
-  @Expose()
   public name!: string;
 }
--- frontend/src/dto/user/dto/update-user.dto.ts
+++ frontend/src/dto/user/dto/update-user.dto.ts
@@ -1,12 +1,4 @@
-import { Length } from 'class-validator';
-import { UserNameLength } from '../../../const/index.js';
-
 export default class UpdateUserDto {
-  @Length(
-    UserNameLength.Min, UserNameLength.Max,
-    {message: `Min length for password is ${UserNameLength.Min}, max is ${UserNameLength.Max}`}
-  )
   public name?: string;
-
   public avatarUrl?: string;
 }
